68cae39b61c92b67146f97a6bf3a87583191f1fc
dorce2002 <dorceeditor@gmail.com>
2025-10-29 01:18:01 -0300

    E-mail, Telefone, Site , Integrados com a API

--- src/components/restaurant_page/GeneralInfo.vue
+++ src/components/restaurant_page/GeneralInfo.vue
@@ -25,28 +25,68 @@
         </div>
       </section>
 
+        <section>
+    
+    <div class="grid grid-cols-1 md:grid-cols-2 gap-4">
+      
+      <div v-if="restaurantContactInfo.site" class="flex items-center space-x-3">
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 text-amber-600 flex-shrink-0">
+          <path stroke-linecap="round" stroke-linejoin="round" d="M12 21a9.004 9.004 0 0 0 8.716-6.747M12 21a9.004 9.004 0 0 1-8.716-6.747M12 21c2.485 0 4.5-4.03 4.5-9S14.485 3 12 3m0 18c-2.485 0-4.5-4.03-4.5-9S9.515 3 12 3m0 0a8.997 8.997 0 0 1 7.843 4.582M12 3a8.997 8.997 0 0 0-7.843 4.582m15.686 0A11.953 11.953 0 0 1 12 10.5c-2.998 0-5.74-1.1-7.843-2.918m15.686 0A8.959 8.959 0 0 1 21 12c0 .778-.099 1.533-.284 2.253m0 0A17.919 17.919 0 0 1 12 16.5c-3.162 0-6.133-.815-8.716-2.247m0 0A9.015 9.015 0 0 1 3 12c0-1.605.42-3.113 1.157-4.418" />
+        </svg>
+        <a 
+          :href="restaurantContactInfo.site" 
+          target="_blank" 
+          class="text-gray-700 text-sm hover:text-amber-600 transition truncate"
+        >
+          {{ restaurantContactInfo.site.replace('http://', '').replace('https://', '') || 'SITE' }}
+        </a>
+      </div>
+
+      <div v-if="restaurantContactInfo.number" class="flex items-center space-x-3">
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 text-amber-600 flex-shrink-0">
+          <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 6.75c0 8.284 6.716 15 15 15h2.25a2.25 2.25 0 0 0 2.25-2.25v-1.372c0-.516-.351-.966-.852-1.091l-4.423-1.106c-.44-.11-.902.055-1.173.417l-.97 1.293c-.282.376-.769.542-1.21.38a12.035 12.035 0 0 1-7.143-7.143c-.162-.441.004-.928.38-1.21l1.293-.97c.363-.271.527-.734.417-1.173L6.963 3.102a1.125 1.125 0 0 0-1.091-.852H4.5A2.25 2.25 0 0 0 2.25 4.5v2.25Z" />
+        </svg>
+        <a 
+          :href="`tel:${restaurantContactInfo.number.replace(/\D/g, '')}`" 
+          class="text-gray-700 text-sm hover:text-amber-600 transition"
+        >
+          {{ restaurantContactInfo.number }}
+        </a>
+      </div>
+
+      <div v-if="restaurantContactInfo.menu" class="flex items-center space-x-3">
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 text-amber-600 flex-shrink-0">
+          <path stroke-linecap="round" stroke-linejoin="round" d="M9 12h3.75M9 15h3.75M9 18h3.75m3 .75H18a2.25 2.25 0 0 0 2.25-2.25V6.108c0-1.135-.845-2.098-1.976-2.192a48.424 48.424 0 0 0-1.123-.08m-5.801 0c-.065.21-.1.433-.1.664 0 .414.336.75.75.75h4.5a.75.75 0 0 0 .75-.75 2.25 2.25 0 0 0-.1-.664m-5.8 0A2.251 2.251 0 0 1 13.5 2.25H15c1.012 0 1.867.668 2.15 1.586m-5.8 0c-.376.023-.75.05-1.124.08C9.095 4.01 8.25 4.973 8.25 6.108V8.25m0 0H4.875c-.621 0-1.125.504-1.125 1.125v11.25c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125V9.375c0-.621-.504-1.125-1.125-1.125H8.25ZM6.75 12h.008v.008H6.75V12Zm0 3h.008v.008H6.75V15Zm0 3h.008v.008H6.75V18Z" />
+        </svg>
+        <a 
+          :href="restaurantContactInfo.menu" 
+          target="_blank" 
+          class="text-gray-700 text-sm hover:text-amber-600 transition"
+        >
+          MENU
+        </a>
+      </div>
+
+      <div v-if="restaurantContactInfo.mail" class="flex items-center space-x-3">
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 text-amber-600 flex-shrink-0">
+          <path stroke-linecap="round" stroke-linejoin="round" d="M21.75 6.75v10.5a2.25 2.25 0 0 1-2.25 2.25h-15a2.25 2.25 0 0 1-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0 0 19.5 4.5h-15a2.25 2.25 0 0 0-2.25 2.25m19.5 0v.243a2.25 2.25 0 0 1-1.07 1.916l-7.5 4.615a2.25 2.25 0 0 1-2.36 0L3.32 8.91a2.25 2.25 0 0 1-1.07-1.916V6.75" />
+        </svg>
+        <a 
+          :href="`mailto:${restaurantContactInfo.mail}`" 
+          class="text-gray-700 text-sm hover:text-amber-600 transition truncate"
+        >
+          {{ restaurantContactInfo.mail }}
+        </a>
+      </div>
+      
+    </div>
+  </section>
+
       <section>
         <h2 class="text-2xl font-semibold mb-3">Sobre o Restaurante</h2>
         <p class="text-gray-700">{{ restaurant.description || 'Nenhuma descrição detalhada fornecida.' }}</p>
       </section>
 
-      <section>
-        <h2 class="text-2xl font-semibold mb-3">Contato</h2>
-        <ul class="space-y-1 text-gray-700">
-            <li v-if="restaurantContactInfo.number">
-                📞 Telefone: <a :href="`tel:${restaurantContactInfo.number}`" class="text-yellow-600 hover:underline">{{ restaurantContactInfo.number }}</a>
-            </li>
-            <li v-if="restaurantContactInfo.site">
-                🌐 Site: <a :href="restaurantContactInfo.site" target="_blank" class="text-yellow-600 hover:underline">{{ restaurantContactInfo.site }}</a>
-            </li>
-            <li v-if="restaurantContactInfo.menu">
-                🍽️ Menu: <a :href="restaurantContactInfo.menu" target="_blank" class="text-yellow-600 hover:underline">{{ restaurantContactInfo.menu }}</a>
-            </li>
-            <li v-if="restaurantContactInfo.mail">
-                📧 Email: <a :href="`mailto:${restaurantContactInfo.mail}`" class="text-yellow-600 hover:underline">{{ restaurantContactInfo.mail }}</a>
-            </li>
-        </ul>
-      </section>
     </div>
   </div>
 </template>
